b6fa0ec289f3044e991f41ef7bc94dfa3bcdf7be
alberto90179 <albertoamezquita2015@gmail.com>
2024-11-30 23:09:00 -0600

    pi

--- Peliculas/src/app/movie.service.ts
+++ Peliculas/src/app/movie.service.ts
@@ -10,3 +10,18 @@ export class MovieService {
 
   constructor() { }
 }
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root' // Esto asegura que el servicio esté disponible en toda la aplicación
+})
+export class MoviesService {
+  private apiUrl = 'https://localhost/movies';
+
+  constructor(private http: HttpClient) { }
+
+  getMovies() {
+    return this.http.get(this.apiUrl);
+  }
+}
