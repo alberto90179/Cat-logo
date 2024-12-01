87fb8db94b80bc02f598e968bcea4a5df2cc9ae9
alberto90179 <albertoamezquita2015@gmail.com>
2024-11-30 23:38:27 -0600

    PI

--- Peliculas/src/app/app.module.ts
+++ Peliculas/src/app/app.module.ts
@@ -1,2 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+@Component({
+  standalone: true, // Esto lo hace standalone
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterModule], // Importa módulos necesarios aquí
+})
 
+export class AppComponent {}
--- Peliculas/src/app/app.routes.ts
+++ Peliculas/src/app/app.routes.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MoviesComponent } from './movies/movies.component';
-import { MovieComponent } from './movie/movie.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { EditMovieComponent } from './components/edit-movie/edit-movie.component';
 
-export const routes: Routes = [ // Export the routes variable
-  { path: 'movies', component: MoviesComponent },
-  { path: '', redirectTo: '/movies', pathMatch: 'full' },
-  { path: 'movie/:id', component: MovieComponent }
+export const routes: Routes = [
+    { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+    { path: 'catalog', component: CatalogComponent },
+    { path: 'movie/:id', component: MovieDetailComponent },
+    { path: 'add', component: AddMovieComponent },
+    { path: 'edit/:id', component: EditMovieComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
